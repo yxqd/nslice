fixed bugs (angle units) and a sign of kf z component. added a generic "slice" method

nslice/slice.py
@@ -3,13 +3,34 @@
 
 import numpy as np
 
-def slice(hklEIE, **kwds):
+def slice(hklEIE, x=None, y=None, u=None, v=None,
+          h=None, k=None, l=None, E=None):
     """
-    slice(hklEIE, x='h', y='E', k=[-0.1,0.1], l=[None,None])
+    slice(hklEIE,
+        x='h', y='E', u='k', v='l',
+        k=[0.95,1.05], l=[-1,1],
+        h=(-3, 5.8, 0.02), E=(-5, 10, 0.1),
+        )
     """
-    xaxisname = kwds['x']
-    yaxisname = kwds['y']
-    return
+    xmin, xmax, dx = eval(x)
+    ymin, ymax, dy = eval(y)
+
+    umin, umax = eval(u)
+    vmin, vmax = eval(v)
+    
+    h, k, l, E, I, error = hklEIE
+    data = hklEIE[:, (eval(u)>umin)*(eval(u)<umax)*(eval(v)>vmin)*(eval(v)<vmax)]
+    
+    I[I!=I] = 0 # remove nans in intensity
+    I[I<0] = 0
+    sample = np.vstack((eval(x), eval(y))).T
+    bins = [
+        np.arange(xmin, xmax+dx/2, dx),
+        np.arange(ymin, ymax+dy/2, dy),
+        ]
+    weights = I
+    H, edges = np.histogramdd(sample, bins=bins, weights=weights)
+    return H, edges
 
 
 def slice_hE(hklEIE, k=None, l=None, h=None, E=None):
@@ -22,6 +43,7 @@ def slice_hE(hklEIE, k=None, l=None, h=None, E=None):
     data = hklEIE[:, (k>kmin)*(k<kmax)*(l>lmin)*(l<lmax)]
     
     I[I!=I] = 0 # remove nans in intensity
+    I[I<0] = 0
     sample = np.vstack((h, E)).T
     bins = [
         np.arange(hmin, hmax+dh/2, dh), 

nslice/spe2hkle.py
@@ -43,7 +43,7 @@ def spe2hkle(Ei, Ef, theta, phi, mat):
     # each component has shape: npix, nEf
     kfx = np.cos(theta)[:, np.newaxis] * kf[np.newaxis, :]
     kfy = (np.sin(theta) * np.cos(phi))[:, np.newaxis] * kf[np.newaxis, :]
-    kfz = (np.sin(theta) * np.sin(phi))[:, np.newaxis] * kf[np.newaxis, :]
+    kfz = (-np.sin(theta) * np.sin(phi))[:, np.newaxis] * kf[np.newaxis, :]
     # kf vector
     kfx.shape = kfy.shape = kfz.shape = -1,
     kfv = np.vstack((kfx, kfy, kfz)).T