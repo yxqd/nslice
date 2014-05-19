5e9f432833daafbe0c2c6ca47c5d4216795ced92
Jiao Lin <linjiao@caltech.edu>
2014-05-19 07:33:57 -0700

    slice.py: new method slice_output_dims

--- nslice/slice.py
+++ nslice/slice.py
@@ -33,6 +33,26 @@ def slice(hklEIE, x=None, y=None, u=None, v=None,
     return H, edges
 
 
+def slice_output_dims(
+    hklEIE, x=None, y=None, u=None, v=None,
+    h=None, k=None, l=None, E=None):
+    """
+    slice_output_dims(hklEIE,
+        x='h', y='E', u='k', v='l',
+        k=[0.95,1.05], l=[-1,1],
+        h=(-3, 5.8, 0.02), E=(-5, 10, 0.1),
+        )
+    """
+    xmin, xmax, dx = eval(x)
+    ymin, ymax, dy = eval(y)
+
+    bins = [
+        np.arange(xmin, xmax+dx/2, dx),
+        np.arange(ymin, ymax+dy/2, dy),
+        ]
+    return (bins[0].size-1, bins[1].size-1), bins
+
+
 def slice_hE(hklEIE, k=None, l=None, h=None, E=None):
     kmin,kmax = k
     lmin,lmax = l
